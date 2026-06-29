27de89f0ee08c79054c88d8f86f75fae2a074b72
CordysCRM Dev <dev@cordys-crm.local>
2026-06-29 21:04:20 +0800

    v1.7.2 upstream

--- backend/framework/src/main/java/cn/cordys/security/ShiroFilter.java
+++ backend/framework/src/main/java/cn/cordys/security/ShiroFilter.java
@@ -97,8 +97,6 @@ public final class ShiroFilter {
         FILTER_CHAIN_DEFINITION_MAP.put("/organization/settings/third-party/get/**", "anon");
         FILTER_CHAIN_DEFINITION_MAP.put("/organization/settings/third-party/sync/resource", "anon");
         FILTER_CHAIN_DEFINITION_MAP.put("/license/validate/**", "anon");
-        FILTER_CHAIN_DEFINITION_MAP.put("/mcp/**", "anon");
-        FILTER_CHAIN_DEFINITION_MAP.put("/opportunity/stage/get", "anon");
     }
 
     /**
